de2d043cf57ed69abac85dee6108eb06a0ddd94c
Kristian Irish M. Balle <KristianIrish.Balle@cebupacificair.com>
2025-08-28 18:13:48 +0800

    refactor

--- step_definitions/API_unknown.js
+++ step_definitions/API_unknown.js
@@ -1,34 +1,30 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
-const chai = require('chai');
-const axios = require('axios');
-const expect = chai.expect;
+import { Given, When, Then } from '@cucumber/cucumber';
+import { expect } from 'chai';
+import axios from 'axios';
 
 let response;
 let endpoint = '';
+let responseTrueRed;
 
 Given('I have the API endpoint for api unknown', function () {
   endpoint = 'https://reqres.in/api/unknown';
 });
 
 When('I send a GET request with to retrieve unknown data', async function () {
-  
-  // try {
-  //   response = await axios({
-  //     method: 'get',
-  //     url: endpoint,
-  //   }); 
-  // } catch (err) {
-  //     // Handle error - you can log the error or access err.response for more details
-  //     response = err.response;
-  //     console.error('Error occurred:', err.response || err.message);
-  //   }
-  //   responseTrueRed = response.data.data.filter(item => item.name.toLowerCase() === 'true red')[0];
-  })
+  try {
+    response = await axios.get(endpoint);
+  } catch (err) {
+    response = err.response;
+    console.error('Error occurred:', err.response || err.message);
+  }
+
+  responseTrueRed = response.data.data.find(item => item.name.toLowerCase() === 'true red');
+});
 
 Then('I should receive a good status code', function () {
   expect(response.status).to.equal(200);
 });
 
 Then('the color of true red is {string}', function (str) {
-  expect(responseTrueRed.color).to.eq(str);
+  expect(responseTrueRed.color).to.equal(str);
 });
